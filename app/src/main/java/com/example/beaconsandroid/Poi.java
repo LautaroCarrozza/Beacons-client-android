6eaacff4a1efbf2647587e4080113f5505558d5a
lautarocarrozza <lautaro.carrozza@ing.austral.edu.ar>
2019-11-20 13:26:23 -0300

    Documentation finished.

--- app/src/main/java/com/example/beaconsandroid/Poi.java
+++ app/src/main/java/com/example/beaconsandroid/Poi.java
@@ -4,10 +4,16 @@ public class Poi {
     private String title;
     private String htmlContent;
 
+    /**
+     * Constructor creates an instance of {@link Poi}
+     * @param title title of the point of interest to create
+     * @param htmlContent poi body content in html format
+     */
     public Poi(String title, String htmlContent) {
         this.title = title;
         this.htmlContent = htmlContent;
     }
+
 
     public String getTitle() {
         return title;
@@ -25,6 +31,11 @@ public class Poi {
         this.htmlContent = htmlContent;
     }
 
+    /**
+     * Compares whether some other poi is "equal to" this one
+     * @param obj the reference object with wich to compare
+     * @return {@code true} if this object is the same as the obj to compare
+     */
     @Override
     public boolean equals(Object obj){
         if(obj instanceof Poi){
